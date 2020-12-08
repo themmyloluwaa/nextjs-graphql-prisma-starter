fb3e4aa019e98a076445635670bcac217e39e38f
Temiloluwa Philip Ojo <codekagei@gmail.com>
2020-12-08 03:19:22 +0100

    added ApolloProvider and client to wrap app component

--- Frontend/pages/_app.js
+++ Frontend/pages/_app.js
@@ -1,6 +1,14 @@
 // pages/_app.js
+import { ApolloProvider } from "@apollo/client";
+import { useApollo } from "../src/utils/apolloClient";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps }) {
+  const apolloClient = useApollo(pageProps);
+
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
